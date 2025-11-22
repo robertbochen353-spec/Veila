91617f87b020f4ecb7f346f1a074ece411b833f1
robertbochen353 <robertbochen353@gmail.com>
2025-11-22 12:22:28 +0800

    Configure for GitHub Pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,10 +22,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://robertbochen353-spec.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Veila/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
